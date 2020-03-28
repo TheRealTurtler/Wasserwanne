4e522575ace3b45e1cfbd276a5ec6fe753823ccc
TheRealTurtler <michael.diedler.7399@gmail.com>
2020-03-28 18:37:48 +0100

    Initial commit

---
+++ Wasserwanne/main.c
@@ -0,0 +1,18 @@
+/*
+ * Wasserwanne.c
+ *
+ * Created: 28.03.2020 18:34:38
+ * Author : Michael
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
